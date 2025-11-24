161f35ee866524da1243b90acf546e955d92009f
Brett Blomberg <49074821+BrettBlomb@users.noreply.github.com>
2025-11-24 12:14:25 -0800

    Update test_google_search.py

--- tests/test_google_search.py
+++ tests/test_google_search.py
@@ -1,11 +1,10 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
-def test_duckduckgo_search():
+def test_login_flow():
     chrome_options = Options()
     chrome_options.add_argument("--headless=new")
     chrome_options.add_argument("--no-sandbox")
@@ -14,12 +13,15 @@ def test_duckduckgo_search():
     driver = webdriver.Chrome(options=chrome_options)
     wait = WebDriverWait(driver, 10)
 
-    driver.get("https://duckduckgo.com/")
+    driver.get("https://the-internet.herokuapp.com/login")
 
-    search_box = wait.until(EC.presence_of_element_located((By.ID, "search_form_input_homepage")))
-    search_box.send_keys("SDET jobs" + Keys.RETURN)
+    # Enter login credentials
+    wait.until(EC.visibility_of_element_located((By.ID, "username"))).send_keys("tomsmith")
+    wait.until(EC.visibility_of_element_located((By.ID, "password"))).send_keys("SuperSecretPassword!")
+    wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, "button.radius"))).click()
 
-    results = wait.until(EC.presence_of_all_elements_located((By.CSS_SELECTOR, "#links .result")))
-    assert len(results) > 0, "No search results found!"
+    # Validate login success
+    message = wait.until(EC.visibility_of_element_located((By.ID, "flash")))
+    assert "You logged into a secure area!" in message.text
 
     driver.quit()
